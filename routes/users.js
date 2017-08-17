354e6db83f7e4b3abc72f29d104f6c924ab5b0e3
hariantara <hariantara.iputu@gmail.com>
2017-08-17 09:06:27 +0700

    rating and search feature done

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.get('/rating/:idU/:idM', function(req,res) {
 })
 
 router.post('/rating/:idU/:idM', function(req,res) {
-  db.MovieUser.update({
+  db.MovieUser.create({
     Rating: `${req.body.rating}`,
     UserId: `${req.params.idU}`,
     MovieId: `${req.params.idM}`
@@ -41,5 +41,27 @@ router.post('/rating/:idU/:idM', function(req,res) {
   .then(() =>{
     res.redirect('/users')
   })
+})
+
+router.post('/search', (req, res)=>{
+  //let search = req.body.search
+  // let lower = search.toLowerCase()
+  db.User.findAll()
+  .then(data=>{
+    db.Movie.findAll({
+      where:{
+        movie_name:{$like: `%${req.body.search}%`}
+        // release_year:{$like: `%${req.body.search}%`},
+        // synopsis:{$like: `%${req.body.search}%`},
+        // company:{$like: `%${req.body.search}%`}
+        // producer:{$like: `%${req.body.search}%`}
+      }
+    })
+    .then(data2=>{
+      console.log("====>",data2);
+      res.render('result-search', {userData:data,movieName:data2})
+    })
+  })
+
 })
 module.exports = router;
